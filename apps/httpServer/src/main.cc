6e45b1065392af752fa5e86540bfc9618260acee
Ma Aiguo <imaiguo@gmail.com>
2023-10-19 14:11:34 +0800

    apps added

---
+++ apps/httpServer/src/main.cc
@@ -0,0 +1,111 @@
+// Copyright (c) 2020 Cesanta Software Limited
+// All rights reserved
+
+#include <signal.h>
+
+extern "C"
+{
+    #include "mongoose.h"
+}
+
+static int s_debug_level = MG_LL_INFO;
+static const char *s_root_dir = ".";
+static const char *s_listening_address = "http://0.0.0.0:8000";
+static const char *s_enable_hexdump = "no";
+static const char *s_ssi_pattern = "#.html";
+
+// Handle interrupts, like Ctrl-C
+static int s_signo;
+
+static void signal_handler(int signo) {
+  s_signo = signo;
+}
+
+// Event handler for the listening connection.
+// Simply serve static files from `s_root_dir`
+static void cb(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+  if (ev == MG_EV_HTTP_MSG) {
+    struct mg_http_message *hm = (mg_http_message *)ev_data, tmp = {0};
+    struct mg_str unknown = mg_str_n("?", 1), *cl;
+    struct mg_http_serve_opts opts = {0};
+    opts.root_dir = s_root_dir;
+    opts.ssi_pattern = s_ssi_pattern;
+    mg_http_serve_dir(c, hm, &opts);
+    mg_http_parse((char *) c->send.buf, c->send.len, &tmp);
+    cl = mg_http_get_header(&tmp, "Content-Length");
+    if (cl == NULL) cl = &unknown;
+    MG_INFO(("%.*s %.*s %.*s %.*s", (int) hm->method.len, hm->method.ptr,
+             (int) hm->uri.len, hm->uri.ptr, (int) tmp.uri.len, tmp.uri.ptr,
+             (int) cl->len, cl->ptr));
+  }
+  (void) fn_data;
+}
+
+static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+  if (ev == MG_EV_CONNECT) {
+    struct mg_str host = mg_url_host(s_listening_address);
+    // Send request
+    mg_printf(c,
+              "GET %s HTTP/1.0\r\n"
+              "Host: %.*s\r\n"
+              "\r\n",
+              mg_url_uri(s_listening_address), (int) host.len, host.ptr);
+  } if (ev == MG_EV_HTTP_MSG) {
+    struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+    printf("%.*s", (int) hm->message.len, hm->message.ptr);
+  }
+}
+
+static void fn_hello(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+  if (ev == MG_EV_HTTP_MSG) {
+    struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+    if (mg_http_match_uri(hm, "/api/hello")) {              // On /api/hello requests,
+      mg_http_reply(c, 200, "", "{%m:%d}\n",
+                    MG_ESC("status"), 1);                   // Send dynamic JSON response
+    } else {                                                // For all other URIs,
+      struct mg_http_serve_opts opts={0};                       // Serve files
+      opts.root_dir = ".";
+      mg_http_serve_dir(c, hm, &opts);                      // From root_dir
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  char path[MG_PATH_MAX] = ".";
+  struct mg_mgr mgr;
+  struct mg_connection *c;
+
+  // Root directory must not contain double dots. Make it absolute
+  // Do the conversion only if the root dir spec does not contain overrides
+  if (strchr(s_root_dir, ',') == NULL) {
+    realpath(s_root_dir, path);
+    s_root_dir = path;
+  }
+
+  // Initialise stuff
+  signal(SIGINT, signal_handler);
+  signal(SIGTERM, signal_handler);
+
+  mg_log_set(s_debug_level);
+
+  mg_mgr_init(&mgr);
+
+  if ((c = mg_http_listen(&mgr, s_listening_address, fn_hello, &mgr)) == NULL) {
+    MG_ERROR(("Cannot listen on %s. Use http://ADDR:PORT or :PORT",
+              s_listening_address));
+
+    exit(EXIT_FAILURE);
+  }
+
+  if (mg_casecmp(s_enable_hexdump, "yes") == 0) c->is_hexdumping = 1;
+
+  // Start infinite event loop
+  MG_INFO(("Mongoose version : v%s", MG_VERSION));
+  MG_INFO(("Listening on     : %s", s_listening_address));
+  MG_INFO(("Web root         : [%s]", s_root_dir));
+  while (s_signo == 0) mg_mgr_poll(&mgr, 1000);
+
+  mg_mgr_free(&mgr);
+  MG_INFO(("Exiting on signal %d", s_signo));
+  return 0;
+}
